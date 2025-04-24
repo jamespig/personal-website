3e6095214f09a158a8cee3bcba9657c61e947c55
jamespig <james7766646@gmail.com>
2025-04-24 14:24:31 +0800

    feat: 加入Logo、變動title

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,10 +9,19 @@ const routes: RouteRecordRaw[] = [
     path: "/",
     name: "home",
     component: () => import("../pages/index.vue"),
+    meta: { title: "Homepage-JamesPig" },
   },
 ];
 
-export const router = createRouter({
+const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
+router.beforeEach((to, _from, next) => {
+  const defaultTitle = "JamesPig";
+  document.title = (to.meta.title as string) || defaultTitle;
+  next();
+});
+
+export default router;
