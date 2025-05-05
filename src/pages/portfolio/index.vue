b1613ca406b3803d7dd4186ccfc4f70e2353e1f3
jamespig <james7766646@gmail.com>
2025-05-05 16:21:44 +0800

    feat: 更新GSAP套件、實作導覽列

--- src/pages/portfolio/index.vue
+++ src/pages/portfolio/index.vue
@@ -1,7 +1,9 @@
 <template>
-  <div></div>
+  <div class="flex flex-col h-screen bg-[#6E163C]">
+    <img :src="portfolio" loading="lazy" alt="portfolio" class="w-auto h-45" />
+  </div>
 </template>
 
-<script lang="ts" setup></script>
-
-<style lang="scss" scoped></style>
+<script lang="ts" setup>
+import portfolio from "../../assets/images/imagery/Portfolio.svg";
+</script>
